3e2d49f0179702842b8fb4d031e07678e5b85b78
EC2 Default User <ec2-user@ip-172-31-93-118.ec2.internal>
2020-04-14 02:07:27 +0000

    user update without password

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   root to: 'home#index'
-  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+  devise_for :users, controllers: { 
+    omniauth_callbacks: 'users/omniauth_callbacks',
+    registrations: 'users/registrations'
+  }
+
   resources :users, only:[:index, :show, :edit, :update]
 end
