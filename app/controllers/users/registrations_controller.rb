7e543ee21db84b6a40681486c0b45af3fb850ebd
EC2 Default User <ec2-user@ip-172-31-93-118.ec2.internal>
2020-04-14 02:46:51 +0000

    fix user registrations_controller

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -63,10 +63,5 @@ class Users::RegistrationsController < Devise::RegistrationsController
   def after_inactive_sign_up_path_for(resource)
     super(resource)
   end
-  
-  def build_resource(hash = nil)  
-    hash[:uid] = User.create_unique_string  
-    super  
-  end
-  
+
 end
