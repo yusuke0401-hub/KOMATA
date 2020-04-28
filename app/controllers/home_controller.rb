e8883c0754bf7a8cc22de15f637c9b56251617cb
EC2 Default User <ec2-user@ip-172-31-93-118.ec2.internal>
2020-04-28 05:01:10 +0000

    otasuke_message_ranking_users

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,5 @@
 class HomeController < ApplicationController
   def index
+    @otasuke_message_ranking_users = User.find(OtasukeMessage.group(:user_id).order('count(user_id) DESC').limit(3).pluck(:user_id))
   end
 end
