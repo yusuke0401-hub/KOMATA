664f3cd89dac6cbf8fcaffcd3cf0ca084c689cd7
EC2 Default User <ec2-user@ip-172-31-93-118.ec2.internal>
2020-05-02 02:23:24 +0000

    avatar destroy

---
+++ app/assets/javascripts/common.js
@@ -0,0 +1,21 @@
+/* global $ */
+
+// avatar画像を即時プレビュー
+$( document ).on('turbolinks:load', function() {
+  function readURL(input) {
+    if (input.files && input.files[0]) {
+      var reader = new FileReader();
+ 
+      reader.onload = function (e) {
+        $('#avatar_img_prev').attr('src', e.target.result);
+      }
+      reader.readAsDataURL(input.files[0]);
+    }
+  }
+ 
+  $("#post_img").change(function(){
+    $('#avatar_img_prev').removeClass('hidden');
+    $('.avatar_present_img').remove();
+    readURL(this);
+  });
+});
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,7 +16,7 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
    devise_parameter_sanitizer.permit(:sign_in, keys: [:name, :email, :password, :password_confirmation])  
    devise_parameter_sanitizer.permit(:sign_up, keys: [:login, :name, :email, :password])
-   devise_parameter_sanitizer.permit(:account_update, keys: [:name, :profile, :avatar])
+   devise_parameter_sanitizer.permit(:account_update, keys: [:name, :profile, :avatar, :avatar_cache, :remove_avatar])
   end
   
 end
